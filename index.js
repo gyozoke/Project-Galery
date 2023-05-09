a095e2a8dcdfacd999a57174f8f5c9d8c38b52d0
Victor Gyozo Vancsura <gyozovancsura@gmail.com>
2023-05-09 14:50:08 +0100

    added javascript function, finalised styling in css, added more content in html

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ var yyyy = today.getFullYear();
 
 today = mm + '/' + dd + '/' + yyyy;
 document.write(today);
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+function myFunction(el) {
+    el.nextSibling.nextSibling.classList.toggle("show");
+    console.log(el);
+    }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
