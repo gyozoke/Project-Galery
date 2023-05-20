207d67eb4eec4f03cace67f1bb33c1d33672fa05
Victor Gyozo Vancsura <gyozovancsura@gmail.com>
2023-05-20 13:11:37 +0100

    Updated date and logo

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = today.getFullYear();
 
 today = mm + '/' + dd + '/' + yyyy;
-document.write(today);
+document.getElementById('date').innerHTML = today;
 
 
 /* When the user clicks on the button, 
